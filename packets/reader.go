0339cd551d9c5b49bbad099a631501e648ce2fe0
lenforiee <lenforiee@gmail.com>
2022-01-08 03:19:38 +0100

    [+] more packet stuff

--- packets/reader.go
+++ packets/reader.go
@@ -1,8 +1,9 @@
 package packets
 
 import (
-	"fmt"
-	bin "github.com/roman-kachanovsky/go-binary-pack/binary-pack"
+	"bytes"
+	"math"
+	"encoding/binary"
 )
 
 type Packet struct {
@@ -15,7 +16,7 @@ type Packet struct {
 var HEADER_LEN = 7 
 
 // Create new packet.
-func newPacket(buffer []byte) (packet Packet) {
+func NewPacketReader(buffer []byte) (packet Packet) {
 	packet.packetBuffer = buffer
 	packet.offset = 0
 	packet.packetID = 0
@@ -24,18 +25,142 @@ func newPacket(buffer []byte) (packet Packet) {
 	return packet
 }
 
-func (p Packet) Read(size int) (data []byte) {
+func ReadInt(b []byte) int {
+	buf := bytes.NewBuffer(b)
+
+	// I wanna hide this code somewhere so 
+	// i dont have to look at it...
+	switch len(b) {
+	case 1:
+		var x int8
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	case 2:
+		var x int16
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	case 4:
+		var x int32
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	default:
+		var x int64
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	}
+}
+
+func ReadUint(b []byte) int {
+	buf := bytes.NewBuffer(b)
+
+	// I wanna hide this code somewhere so 
+	// i dont have to look at it...
+	switch len(b) {
+	case 1:
+		var x uint8
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	case 2:
+		var x uint16
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	case 4:
+		var x uint32
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	default:
+		var x uint64
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	}
+}
+
+func (p *Packet) Read(size int) (data []byte) {
 	data = p.packetBuffer[p.offset:p.offset+size]
-	p.offset = (p.offset + size)
+	p.offset += size
 	return data
 }
 
-func (p Packet) ReadHeader() {
+func (p *Packet) ReadHeader() {
 	headerData := p.Read(HEADER_LEN)
-	bp := new(bin.BinaryPack)
-	unpack, err := bp.UnPack([]string{"H", "I"}, headerData)
-	if err != nil {
-		return
+	p.packetID = ReadInt(headerData[:2])
+	p.packetLen = ReadInt(headerData[:4])
+}
+
+func (p *Packet) MultiPacketCheck() {
+	// Checks if the packet has additional data.
+	if len(p.packetBuffer) > (p.packetLen + HEADER_LEN) {
+		p.packetBuffer = p.packetBuffer[:HEADER_LEN + p.packetLen]
 	}
-	fmt.Println(unpack)
+}
+
+func (p *Packet) ReadUint8() int {
+	return ReadUint(p.Read(1))
+}
+
+func (p *Packet) ReadInt8() int {
+	return ReadInt(p.Read(1))
+}
+
+func (p *Packet) ReadI32List() (vars []int) {
+	length := p.ReadUint16()
+	for i := 0; i < length; i++ {
+		vars = append(vars, p.ReadInt32())
+	}
+	return vars
+}
+
+func (p *Packet) ReadUint16() int {
+	return ReadUint(p.Read(2))
+}
+
+func (p *Packet) ReadInt16() int {
+	return ReadInt(p.Read(2))
+}
+
+func (p *Packet) ReadUint32() int {
+	return ReadUint(p.Read(4))
+}
+
+func (p *Packet) ReadInt32() int {
+	return ReadInt(p.Read(4))
+}
+
+func (p *Packet) ReadUint64() int {
+	return ReadUint(p.Read(8))
+}
+
+func (p *Packet) ReadInt64() int {
+	return ReadInt(p.Read(8))
+}
+
+func (p *Packet) ReadFloat64() float64 {
+    bits := binary.LittleEndian.Uint64(p.Read(8))
+    float := math.Float64frombits(bits)
+    return float
+}
+
+func (p *Packet) ReadFloat32() float32 {
+    bits := binary.LittleEndian.Uint32(p.Read(4))
+    float := math.Float32frombits(bits)
+    return float
+}
+
+func (p *Packet) ReadOsuString() string {
+	if p.ReadUint8() != 0x0B {
+		return ""
+	}
+
+	var val int
+	var shift uint
+	for {
+		b := p.ReadUint8()
+		val |= (int(b & 0x7F) << shift)
+		if b & 0x80 == 0 {
+			break
+		}
+		shift += 7
+	}
+
+	return string(p.Read(val))
 }
--- packets/writer.go
+++ packets/writer.go
@@ -1,5 +1,12 @@
 package packets
 
+import (
+	"fmt"
+	"bytes"
+	"encoding/binary"
+	"reflect"
+)
+
 func WriteUleb128(value int) (ret []byte) {
 	var len int
 
@@ -10,14 +17,13 @@ func WriteUleb128(value int) (ret []byte) {
 
 	for value > 0 {
 		ret = append(ret, 0) // Stupid hack.
-		ret[len] = byte(value & 127)
+		ret[len] = byte(value & 0x7F)
 		value >>= 7
 		if value != 0 {
-			ret[len] |= 128
+			ret[len] |= 0x80
 		}
 		len++
 	}
-
 	return ret
 }
 
@@ -26,10 +32,33 @@ func WriteOsuString(value string) (ret []byte) {
 	if value == "" {
 		ret = []byte{0}
 	} else {
-		ret = append(ret, "\x0B"...) // This is cursed.
-		ret = append(ret[:], WriteUleb128(len(value))[:]...)
-		ret = append(ret, value...)
+		b := []byte(value)
+		ret = append(ret, 0x0B) // This is cursed.
+		ret = append(ret, WriteUleb128(len(b))...)
+		ret = append(ret, b...)
 	}
 
 	return ret
+}
+
+func BuildPacket(packetID int, contents ...interface{}) []byte {
+
+	var packData []byte
+	for _, content := range contents {
+		switch typ := content.(type) {
+		case *string, *[]byte, *[]int32, *[]uint32:
+			BuildPacket(packetID, reflect.TypeOf(typ).Elem())
+		case string:
+			packData = append(packData, WriteOsuString(fmt.Sprint(content))...)
+		}
+	}
+
+	packetLen := len(packData)
+	b := new(bytes.Buffer)
+	binary.Write(b, binary.LittleEndian, packetID)
+	binary.Write(b, binary.LittleEndian, int8(0))
+	binary.Write(b, binary.LittleEndian, packetLen)
+	binary.Write(b, binary.LittleEndian, packData)
+
+	return b.Bytes()
 }
